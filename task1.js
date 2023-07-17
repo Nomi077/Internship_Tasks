f2109ab2d0c261bed9c92f9cf32febc39ae07b69
Nomi077 <122731043+Nomi077@users.noreply.github.com>
2023-07-17 17:13:06 +0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,40 @@
+var myname = "Muhammad Nouman Altaf "; 
+console.log(myname) //Muhammad Nouman Altaf 
+var age = 21; 
+console.log(age) //21
+var student = true;
+var isStudent = (`You are student or not:  ${student}` )
+console.log(isStudent); //You are student or not:  true
+var useless= null;
+console.log(typeof(useless))//object
+var x;
+console.log(x);//undefined
+console.log(typeof(x));//undefined
+var myArray=["name",12,true]
+console.log(myArray)//[ 'name', 12, true ]
+var myObject = {name: "Muhammad Nouman Altaf", age: 21, isStudent: true};
+console.log(myObject);
+console.log(typeof(myObject));
+var num1=20;
+var num2=30;
+var sum = num1+num2;
+console.log(sum);//50
+const PI= 3.14;
+console.log(PI) ;//3.14
+var message = "String1";
+console.log(message);//String1
+var message = "String2";
+console.log(message);//String2
+var fullName = "Muhammad nouman " + " AlTaf";
+var firstName="Muhammad Nouman ";
+var lastName=" Altaf";
+console.log(firstName +" "+lastName);//Muhammad nouman AlTaf
+var isStudent = Math.sqrt(-3);
+console.log(isStudent);//NaN
+var myFunction = function x(){console.log("hello")};
+console.log(typeof(myFunction));//number
+
+
+
+
+
