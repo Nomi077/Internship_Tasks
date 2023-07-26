bb0d880cf5375ff04143815c62603f83e50216d4
Nomi077 <122731043+Nomi077@users.noreply.github.com>
2023-07-26 20:24:54 +0500

    Add files via upload

---
+++ Project4js/script.js
@@ -0,0 +1,18 @@
+const tempload = () => {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = "&#xf2cb";
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+  }, 1000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+  }, 2000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+  }, 3000);
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+  }, 4000);
+};
+tempload();
+setInterval(tempload, 5000);
